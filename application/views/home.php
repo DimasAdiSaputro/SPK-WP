e7e4b32a93cb930bba842afa9dd061a6f146414b
DimasAdiSaputro <dimasadisaputr0699@gmail.com>
2023-11-08 23:02:05 +0700

    first commit

---
+++ application/views/home.php
@@ -0,0 +1,15 @@
+<?php $this->load->view('template/header'); ?>
+
+<div class="box box-success">
+    <div class="box-header with-border">
+        <h3 class="box-title">kelompok 4</h3>
+        <h1><center><bold>SELAMAT DATANG DI APLIKASI PENENTUAN RUMAH MAKAN PADANG JAKARTA PUSAT PADA APLIKASI GRABFOOD</bold></center></h1>
+    </div>
+    <div class="box-body">
+    	<center>
+             <img src="assets/images/RM.jpg" width="500" heiht="500"> 
+        <center>
+        <h2><marquee width="50%">Silahkan pilih menu disamping untuk melakukan pengolahan data<marquee width="50%"></h2>
+    </div>
+</div>  
+
